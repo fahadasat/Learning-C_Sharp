f1708033a28a7cd3a39978e95a8edab316ef4748
fahadasat <asatfahad@gmail.com>
2024-11-07 11:34:13 -0700

    conditionals with "switch"

--- Start/Program Flow/Conditional-switch/Program.cs
+++ Start/Program Flow/Conditional-switch/Program.cs
@@ -9,7 +9,23 @@ namespace ConditionalOps
             int theVal = 50;
 
             // TODO: The switch statement
-
+            switch (theVal) {
+                case 50:
+                    Console.WriteLine("val is 50");
+                    break;
+                case 51:
+                    Console.WriteLine("val is 51");
+                    break;
+                case 55:
+                case 52:
+                case 53:
+                case 54:
+                    Console.WriteLine("val is between 51 and 55");
+                    break;
+                default:
+                    Console.WriteLine("val is not known");
+                    break;
+            }
         }
     }
 }
