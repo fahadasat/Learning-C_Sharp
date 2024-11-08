0eb5bb0d2a1a062eadfdf52ddc5bdb2483fd17cd
fahadasat <asatfahad@gmail.com>
2024-11-07 18:31:49 -0700

    using stringbuilder

--- Start/Strings/Builder/Program.cs
+++ Start/Strings/Builder/Program.cs
@@ -11,31 +11,31 @@ namespace Builder
             string[] animals = {"goats", "cats", "pigs"};
 
             // TODO: create a StringBuilder
-
+            StringBuilder sb = new StringBuilder("Initial String.", 200);
             
             // TODO: print some basic stats about the StringBuilder
-
+            Console.WriteLine($"Capacity: {sb.Capacity}, Length: {sb.Length}");
 
             // TODO: Add some strings to the builder using Append
-
+            sb.Append("Test code");
 
             // TODO: AppendLine can append a line ending
-
+            sb.AppendLine();
 
             // TODO: AppendFormat can be used to append formatted strings
-
+            sb.AppendFormat("he did this {0} times.", jumpCount);
 
             // TODO: AppendJoin can iterate over a set of values
-
+            sb.AppendJoin(',', animals);
 
             // TODO: Modify the content using Replace
-
+            sb.Replace("he", "she");
 
             // TODO: Insert content at any index
-
-            
+            sb.Insert(0, "testing");
+            Console.WriteLine($"Capacity: {sb.Capacity}, Length: {sb.Length}");
             // TODO: Convert to a single string
-
+            Console.WriteLine(sb.ToString());
         }
     }
 }
