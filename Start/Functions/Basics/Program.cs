9f9e3f840b270ff82db1f6504e2086b79a454615
fahadasat <asatfahad@gmail.com>
2024-11-07 19:42:00 -0700

    function basics

--- Start/Functions/Basics/Program.cs
+++ Start/Functions/Basics/Program.cs
@@ -5,12 +5,17 @@
 
 // TODO: Functions have a return type, name, and optional parameters
 
+float MilesToKm(float miles) {
+    return miles * 1.6f;
+}
 
 // TODO: A function with no return value has a 'void' type
-
+void PrintWithPrefix(string str) {
+    Console.WriteLine($"::> {str}");
+}
 
 // TODO: Call first function
-
+Console.WriteLine(MilesToKm(1));
 
 // TODO: Call second function
-
+PrintWithPrefix("Test string");
